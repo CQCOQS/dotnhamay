ecb99f628e489bce9fde88328bf80a460973993d
CQCOQS <86781178+CQCOQS@users.noreply.github.com>
2021-08-02 18:49:54 +0300

    Create Wechat.js

---
+++ Wechat/Wechat.js
@@ -0,0 +1,11 @@
+$('#btn-open').click(function(){
+  window.open("weixin://weixin.qq.com/r/6C_chAXEflgBrUHn93r6");
+});
+
+$('#btn-open-2').click(function(){
+  setTimeout(function(){
+  window.open('weixin://dl/moments')  
+  },1000);
+  
+});
+
