7a521d91a075088160a59c7a5147312649daffeb
CQCOQS <86781178+CQCOQS@users.noreply.github.com>
2021-08-03 13:32:55 +0300

    Create index.php

---
+++ thanhtoan/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__) . '/nusoap/nusoap.php';
+require_once('VDCO_SOAPClient.class.php');
+$urlApi = "https://**********************************************"
+$username = "username****************";
+$password = "password****************";
+$partnerId = "partnerId***";
+$mpin = "mpin***";
+$email = "tt658348@gmail.com";
+$phone = '0384127329';
