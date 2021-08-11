092ee29e55b474bcd30a687ea12f41dd7fd70493
CQCOQS <86781178+CQCOQS@users.noreply.github.com>
2021-08-11 08:52:34 +0300

    Create custom.js

---
+++ myacode/custom.js
@@ -0,0 +1,19 @@
+/*  clock */
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+/*  play button */
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const playBtn = document.querySelector('.circle__btn');
+const wave1 = document.querySelector('.circle__back-1');
+const wave2 = document.querySelector('.circle__back-2');
+
+/*  rate slider */
+const container = document.querySelector('.slider__box');
+const btn = document.querySelector('.slider__btn');
+const color = document.querySelector('.slider__color');
+const tooltip = document.querySelector('.slider__tooltip');
+
+clock = () => {
